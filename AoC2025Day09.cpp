f19fc09dd2a3e4ad1912764ad51e2746fd6a5488
Francois Cayouette <45644908+FCayouette@users.noreply.github.com>
2025-12-09 15:25:50 -0500

    Optimized Day09 C++20 Solution

--- AoC2025Day09.cpp
+++ AoC2025Day09.cpp
@@ -3,8 +3,6 @@ import std;
 #define ALLc(x) (x).cbegin(),(x).cend()
 
 using u64 = unsigned long long;
-
-
 template <typename T>
 constexpr bool IsEqual(T a, T b)
 {
@@ -42,11 +40,15 @@ int main(int argc, char* argv[])
 		return -1;
 	}
 	u64 part1 = 0, part2 = 0;
-	int x, y, minX = 1000000, maxX = 0;
+	int x, y;
 	char c;
-	std::vector<Point> points;
+	std::vector<Point> points, compressed;
 	std::map<u64, std::vector<u64>> outsideCorners;
 	std::set<Point> limits, outside, cur, next;
+	std::set<int> coordX, coordY;
+	std::vector<int> compressedX, compressedY;
+	std::map<int, int> xToCompressed, yToCompressed;
+
 	while (in >> x >> c >> y)
 	{
 		for (const Point& p : points)
@@ -56,26 +58,37 @@ int main(int argc, char* argv[])
 			if (area > part1)
 				part1 = area;
 		}
-		if (!points.empty())
-		{
-			if (x == points.back().x)
-				for (int i = std::min(y, points.back().y); i <= std::max(y, points.back().y); ++i)
-					limits.emplace(x, i);
-			else
-				for (int i = std::min(x, points.back().x); i <= std::max(x, points.back().x); ++i)
-					limits.emplace(i, y);
-		}
 		points.emplace_back(x, y);
-		minX = std::min(x, minX);
-		maxX = std::max(x, maxX);
+		coordX.insert(x);
+		coordY.insert(y);
 	}
-	if (points.back().x == points.front().x)
-		for (int i = std::min(points.front().y, points.back().y); i <= std::max(points.front().y, points.back().y); ++i)
-			limits.emplace(x, i);
-	else
-		for (int i = std::min(points.front().x, points.back().x); i <= std::max(points.front().x, points.back().x); ++i)
-			limits.emplace(i, y);
 	
+	for (int x : coordX)
+	{
+		compressedX.emplace_back(x-1);
+		xToCompressed[x] = compressedX.size();
+		compressedX.emplace_back(x);
+	}
+	compressedX.emplace_back(*coordX.crbegin() + 1);
+	for (int y : coordY)
+	{
+		compressedY.emplace_back(y-1);
+		yToCompressed[y] = compressedY.size();
+		compressedY.emplace_back(y);
+	}
+	compressedY.emplace_back(*coordY.crbegin() + 1);
+
+	for (Point p : points)
+		compressed.emplace_back(xToCompressed[p.x], yToCompressed[p.y]);
+
+	for (int i = 1; i <= compressed.size(); ++i)
+		if (Point a = compressed[i - 1], b = compressed[i % compressed.size()]; a.x == b.x)
+			for (int y = std::min(a.y, b.y), maxY = std::max(a.y, b.y); y <= maxY; ++y)
+				limits.emplace(a.x, y);
+		else
+			for (int x = std::min(a.x, b.x), maxX = std::max(a.x, b.x); x <= maxX; ++x)
+				limits.emplace(x, a.y);
+
 	Point seed = *limits.cbegin() + Point(-1, 0);
 	outside.insert(seed);
 	cur.insert(seed);
@@ -83,7 +96,7 @@ int main(int argc, char* argv[])
 	{
 		for (Point p : cur)
 			for (Point a : allDir)
-				if (Point t = p + a; !limits.contains(t) && !outside.contains(t))
+				if (Point t = p+a; !limits.contains(t) && !outside.contains(t))
 					for (Point d : directions)
 						if (Point np = t + d; limits.contains(np))
 						{
@@ -91,11 +104,10 @@ int main(int argc, char* argv[])
 							outside.insert(t);
 							break;
 						}
-
 		std::swap(cur, next);
 		next.clear();
 	}
-	outsideCorners.clear();
+
 	for (Point p : outside)
 		outsideCorners[p.x].push_back(p.y);
 
@@ -106,20 +118,21 @@ int main(int argc, char* argv[])
 
 	for (int x : toDelete)
 		outsideCorners.erase(x);
-	
-	for (int i = 0; i < points.size() - 1; ++i)
-		for (int j = i + 1; j < points.size(); ++j)
+
+	for (int i = 0; i < compressed.size() - 1; ++i)
+		for (int j = i + 1; j < compressed.size(); ++j)
 		{
-			int mx = std::min(points[i].x, points[j].x), mX = std::max(points[i].x, points[j].x), my = std::min(points[i].y, points[j].y), mY = std::max(points[i].y, points[j].y);
-			u64 area = (u64)(mX - mx + 1) * (u64)(mY - my + 1);
+			int mx = std::min(compressed[i].x, compressed[j].x), mX = std::max(compressed[i].x, compressed[j].x), my = std::min(compressed[i].y, compressed[j].y), mY = std::max(compressed[i].y, compressed[j].y);
+			u64 area = (u64)(compressedX[mX] - compressedX[mx] + 1) * (u64)(compressedY[mY] - compressedY[my] + 1);
 			bool valid = area > part2;
 			for (u64 x = mx; x <= mX && valid; ++x)
 				if (outsideCorners.contains(x))
 					valid = std::find_if(ALLc(outsideCorners[x]), [my, mY](const u64 v) {return my <= v && v <= mY; }) == outsideCorners[x].cend();
 
-			if (valid)
-				part2 = std::max(part2, area);
+			if (valid && area > part2)
+				part2 = area;
 		}
+
 	std::cout << std::format("Part 1: {}\nPart 2: {}\n", part1, part2);
 	std::cout << std::format("Duration: {}\n", std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - ChronoStart));
 }
