1559e919e26e4518cf679aeacf9e7557015ee190
Francois Cayouette <45644908+FCayouette@users.noreply.github.com>
2025-12-20 21:51:47 -0500

    Optimized Day08 C++20 Solution

--- AoC2025Day08.cpp
+++ AoC2025Day08.cpp
@@ -5,20 +5,43 @@ using u64 = unsigned long long;
 struct Point3D
 {
 	constexpr Point3D(int X = 0, int Y = 0, int Z = 0) : x(X), y(Y), z(Z) {}
+	constexpr u64 SquareDist(const Point3D& r) const {
+		long long dx = r.x - x, dy = r.y - y, dz = r.z - z;
+		return dx * dx + dy * dy + dz * dz;
+	}
 	int x, y, z;
 };
-
-u64 SquareDist(const Point3D& a, const Point3D& b)
-{
-	u64 x = std::abs(a.x - b.x), y = std::abs(a.y - b.y), z = std::abs(a.z - b.z);
-	return x * x + y * y + z * z;
-}
 
 struct DistInfo
 {
 	u64 dist;
-	size_t index1, index2;
+	int index1, index2;
 	bool operator >(const DistInfo& r) const { return dist > r.dist; }
+};
+
+template <typename T, std::enable_if_t<std::is_signed_v<T>, bool> = true>
+struct DisjointSet
+{
+	DisjointSet(size_t size) : setInfo(static_cast<T>(size), -1), setCount(size) {}
+
+	T Find(T x)
+	{
+		if (setInfo[x] < 0) return x;
+		return setInfo[x] = Find(setInfo[x]);
+	}
+
+	void Union(T r1, T r2)
+	{
+		if (T i1 = Find(r1), i2 = Find(r2); i1 != i2)
+		{
+			setInfo[i1] += setInfo[i2];
+			setInfo[i2] = i1;
+			--setCount;
+		}
+	}
+	
+	std::vector<T> setInfo;
+	size_t setCount;
 };
 
 int main(int argc, char* argv[])
@@ -37,8 +60,7 @@ int main(int argc, char* argv[])
 		return -1;
 	}
 
-	u64 part1 = 1, part2 = 0;
-	int x, y, z;
+	int part1 = 0, part2 = 0, x, y, z;
 	char c;
 	std::vector<Point3D> points;
 	std::priority_queue<DistInfo, std::vector<DistInfo>, std::greater<DistInfo>> queue;
@@ -46,65 +68,28 @@ int main(int argc, char* argv[])
 	{
 		Point3D p(x, y, z);
 		for (size_t i = 0; i < points.size(); ++i)
-			queue.emplace(SquareDist(points[i], p), i, points.size());
+			queue.emplace(p.SquareDist(points[i]), i, points.size());
 		points.emplace_back(p);
 	}
 
-	std::vector<std::unordered_set<size_t>> circuits;
-
-	auto FindCircuit = [&circuits](size_t index)
-		{
-			for (size_t r = 0; r < circuits.size(); ++r)
-				if (circuits[r].contains(index))
-					return r;
-			return circuits.size();
-		};
-
-	size_t left = points.size(), roundCount =0 ;
+	size_t roundCount = 0;
+	DisjointSet<int> disjoint(points.size());
 	while(true)
 	{
 		++roundCount;
 		DistInfo di = queue.top();
 		queue.pop();
-		size_t a = FindCircuit(di.index1), b = FindCircuit(di.index2);
-		if (a == b)
-		{
-			if (a == circuits.size())
-			{
-				std::unordered_set<size_t> n = { di.index1, di.index2 };
-				circuits.emplace_back(std::move(n));
-				left -= 2;
-			}
-		}
-		else if (a == circuits.size())
-		{
-			circuits[b].insert(di.index1);
-			--left;
-		}
-		else if (b == circuits.size())
-		{
-			circuits[a].insert(di.index2);
-			--left;
-		}
-		else
-		{
-			circuits[a].merge(circuits[b]);
-			if (b < circuits.size() - 1)
-				std::swap(circuits[b], circuits[circuits.size() - 1]);
-			circuits.resize(circuits.size() - 1);
-		}
 
+		disjoint.Union(di.index1, di.index2);
+		
 		if (roundCount == 1000)
 		{
-			std::vector<size_t> sizes;
-			for (const auto& c : circuits)
-				sizes.push_back(c.size());
-
-			std::sort(sizes.begin(), sizes.end(), std::greater());
-			part1 = sizes[0] * sizes[1] * sizes[2];
+			std::vector copy = disjoint.setInfo;
+			std::sort(copy.begin(), copy.end());
+			part1 = std::accumulate(copy.cbegin(), copy.cbegin() + 3, -1ll, std::multiplies());
 		}
 
-		if (!left && circuits.size() == 1)
+		if (disjoint.setCount == 1)
 		{
 			part2 = points[di.index1].x * points[di.index2].x;
 			break;
